3933d220661cd97ce0eabbaf34e6d30e3b4dd34b
Larry-nan <lawrence,yinan@gmail.com>
2019-12-14 23:17:59 -0800

    final touch

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,13 +13,13 @@ connection.connect(err => {
     if (err) throw err;
     console.log(`connected to threadId: ${connection.threadId}`);
     seeProducts();
-    connection.end();
+    //connection.end();
 });
 seeProducts = table => {
-    connection.query('SELECT * FROM products', table, (err, res) => { //Read(crud) CRUD stands for Create-Read-Update-Delete //MYSQL INSERT-SELECT-
+    connection.query('SELECT * FROM products', table, (err, res) => {
         if (err) throw err;
-        storeState = res //variable assignment
-        console.table(res); //ICEBOX look into .toFixed //ICEBOX don't show (index)
+        storeState = res
+        console.table(res);
         buySomething()
     });
 };
@@ -36,27 +36,55 @@ function buySomething() {
             name: 'quantity',
             message: "How many item would you like?",
         }
+
     ]).then(function (input) {
-        console.log(input);
-        console.log(storeState)
+        // console.log(input);
+        // console.log(storeState)
         if (input.quantity <= storeState[input.item_id - 1].stock_quantity) {
             console.log(` there's enough`)
             //actually sell it to them (msyql  query)
-            // connection.query("UPDATE: todos SET?  WHERE ?", [{
-            //     quantity: quantity- sale
-            // }, {
-            //     id: input.id
-            // }],
-            // (err, res) => {
-            //     if (err) throw err;
-            // }
+            let stock_quantity = storeState[input.item_id - 1].stock_quantity
+            let userQty = input.quantity;
+            let userProdId = input.item_id;
+            let inventory = stock_quantity - input.quantity
+            console.log(stock_quantity, inventory, input.item_id)
+            connection.query("UPDATE products SET?  WHERE ?", [{
+                    stock_quantity: inventory
+                }, {
+                    item_id: userProdId
+                }],
+                (err, res) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(res)
+                    // seeProducts()
+                    console.log(totalPrice(userProdId, userQty))
 
+                    connection.end();
+                }
+            )
             //when sale is complete: do you want to buy something else? if yes: seeproducts() else connection.end() process.exit(0)
-
-
         } else {
             console.log(` there's not enough, buy less of that`)
             seeProducts()
+
         }
     })
+}
+
+totalPrice = (userProdId, userQty) => {
+    connection.query("SELECT price From products WHERE ? ;", {
+            item_id: userProdId
+        },
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+            console.log(res[0])
+            return res[0].price * userQty;
+
+        }
+
+    )
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Bamazon App
+---
+## Overview
+---
+### Bamazon is an Amazon-like storefront with the MySQL skills I learned this unit. The app takes in orders from customers and deplete stock from the store's inventory. 
+
+## Customer Portal
+
+### The Bamazon app prompts users with two messages. One that ask the customer the ID of the product they would like to buy. The second message asks how many units of the product they would like to buy.
+### Once the customer has placed the order, the Bamazon app should check if there is enough of the product to meet the customer's request. And show the total cost of the purchase.
